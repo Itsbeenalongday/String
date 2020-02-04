5f5c3d6f4760f14b268fc453f1d0a2612779e071
seongminyoo <dbtjdals1771@ajou.ac.kr>
2020-02-05 01:44:54 +0900

    updating ...

---
+++ string/11656_접미사 배열/11656_접미사 배열.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+bool compare(string a, string b) {
+	return a < b;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	string str;
+	cin >> str;
+	
+	string substr[1005];
+	
+	int length = str.length();
+
+	for (int i = 0; i <= length; ++i) {
+		substr[i] = str.substr(i, length);
+	}
+
+	sort(substr, substr+length, compare);
+
+	for (int i = 0; i <= length; ++i) {
+		cout << substr[i] << '\n';
+	}
+
+	return 0;
+}
